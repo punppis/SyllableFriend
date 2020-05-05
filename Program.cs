7ea93c6cfa5065a5aa835ec9eadfa42adf22c887
Joonas Alhonen <punppis@gmail.com>
2020-05-05 22:18:05 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SyllableFriend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = WordItem.LoadXml("kotus-sanalista_v1/kotus-sanalista_v1.xml");
+            foreach(var word in words.OrderBy(o => Guid.NewGuid()).Take(100))
+            {
+                Console.WriteLine($"Word: {word.Word} ({word.SyllableString})");
+            }
+        }
+    }
+}
